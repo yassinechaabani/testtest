598d7f3398382c7a1000aa290d7c0566238b926d
yassinechaabani <119072810+yassinechaabani@users.noreply.github.com>
2024-05-24 20:18:36 +0100

    Update in prime number(this code is false)

--- demo.py
+++ demo.py
@@ -8,12 +8,12 @@ num = 29
 # define a flag variable
 flag = False
 
-if num == 1:
+if num = 1:
     print(num, "is not a prime number")
-elif num > 1:
+elif num < 1:
     # check for factors
     for i in range(2, num):
-        if (num % i) == 0:
+        if (num % i) =0:
             # if factor is found, set flag to True
             flag = True
             # break out of loop
